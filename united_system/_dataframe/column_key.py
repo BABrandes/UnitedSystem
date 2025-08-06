a9cfadb3b34bcbc9eb7887d2bd2187d0c9be96df
bab <benedikt.brandes@me.com>
2025-08-06 10:42:03 +0200

    fix

--- united_system/_dataframe/column_key.py
+++ united_system/_dataframe/column_key.py
@@ -2,8 +2,8 @@ from typing import Protocol, Type, runtime_checkable
 
 @runtime_checkable
 class ColumnKey(Protocol):
-    def to_string(self) -> str:
+    def to_united_dataframe_string(self) -> str:
         ...
     @classmethod
-    def from_string(cls: Type['ColumnKey'], string: str) -> 'ColumnKey':
+    def from_united_dataframe_string(cls: Type['ColumnKey'], string: str) -> 'ColumnKey':
         ...
